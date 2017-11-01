05046eeebe998ab34efcd82cc373b0fdb21d1ca7
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-11-01 11:24:30 -0500

    more basic setup of authorization files and pug with route structure

---
+++ routes/authRoute.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const { Router } = require("express");
+const router = Router();
+
+const {
+  displayRegister,
+  register,
+  displayLogin,
+  login,
+  welcome,
+  logout
+} = require("../controllers/authCtrl.js");
+
+// new users
+router.get("/register", displayRegister);
+router.post("/register", register);
+
+// login existing users
+router.get("/login", displayLogin);
+router.post("/login", login);
+
+router.get("/welcome", isLoggedIn, welcome);
+router.post("/logout", logout);
+
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/login");
+}
+
+module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.use(require("./authRoute"));
+
+module.exports = router;
